21da33658495d9e6d640dbca81aabb16407df27c
pvansanten <pvansanten@9c705a64-0d33-ca90-3bb0-059f629fa07d>
2010-04-08 09:50:05 +0000

    Frontend: logs

---
+++ trunk/filesystem/usr/local/www/templates/diagnostics/log_boot.tpl.php
@@ -0,0 +1,6 @@
+<h2>Boot log</h2>
+
+<div class="form-error" id="diagnostics_log_boot_error">
+</div>
+
+<code id="diagnostics_log_boot" class="log-file"></code>
---
+++ trunk/filesystem/usr/local/www/templates/diagnostics/log_browser.tpl.php
@@ -0,0 +1,6 @@
+<h2>Browser log</h2>
+
+<div class="form-error" id="diagnostics_log_browser_error">
+</div>
+
+<code id="diagnostics_log_browser" class="log-file"></code>
