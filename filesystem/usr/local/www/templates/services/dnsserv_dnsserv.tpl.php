5a3aad6d3caaa87f4ea1714322eefcacd87fb034
pvansanten <pvansanten@9c705a64-0d33-ca90-3bb0-059f629fa07d>
2010-04-13 13:36:14 +0000

    Bugfix: sharing & dns server

--- filesystem/usr/local/www/templates/services/dnsserv_dnsserv.tpl.php
+++ filesystem/usr/local/www/templates/services/dnsserv_dnsserv.tpl.php
@@ -4,7 +4,7 @@
     <div class="form-error" id="services_dnsserv_form_error">
     </div>
 
-    <input type="hidden" name="module" value="MaraDns"/>
+    <input type="hidden" name="module" value="MaraDNS"/>
     <input type="hidden" name="page" value="save" id="services_dnsserv_form_page"/>
 
     <dl>
