c30aac1498aefb6fd1cf061e2fd3f24b968e7fc9
rickwoelders <rickwoelders@9c705a64-0d33-ca90-3bb0-059f629fa07d>
2010-04-15 09:59:50 +0000

    Bugfix in getSubnet, array to string cast that was unintended.

--- filesystem/usr/local/lib/genericproxy/libs/Interfaces.php
+++ filesystem/usr/local/lib/genericproxy/libs/Interfaces.php
@@ -185,10 +185,12 @@ abstract class Interfaces implements Plugin {
 	 * @access public
 	 */
 	public function getSubnet() {
+		$str_bin = null;
+		
 		$tmp = Functions::shellCommand ( "/sbin/ifconfig " . ( string ) $this->data->if . " | /usr/bin/grep -w \"inet\" | /usr/bin/cut -d\" \" -f 2");
 		$octets_hex = str_split($tmp,2);
 		
-		for ($i=2; $i < strlen($octets_hex); $i++) {
+		for ($i=2; $i < count($octets_hex); $i++) {
     		$str_bin .= decbin(hexdec($octets_hex[$i]));
   		}
   		
